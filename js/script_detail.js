de212a201c50ecadcf59a9a3d69880cd5b995113
datToc004 <datdo200499@gmail.com>
2021-09-06 15:40:18 +0700

    add_script_detail_js

---
+++ js/script_detail.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    var quantitiy = 0;
+    $('.quantity-right-plus').click(function (e) {
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+        $('#quantity').val(quantity + 1);
+
+
+        // Increment
+
+    });
+
+    $('.quantity-left-minus').click(function (e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        if (quantity > 0) {
+            $('#quantity').val(quantity - 1);
+        }
+    });
+
+});
+
+function del_cart(name) {
+    return confirm('Bạn muốn xoá  : ' + name + ' ?');
+}
+
+function update_qty(rowId, qty) {
+    $.get('/update-cart-dish/' + rowId + '/' + qty,
+        function () {
+            window.location.reload();
+        }
+    );
+}
+
+$("#addRow").click(function () {
+    var html = '';
+    html += `<div id="inputFormRow">
+                     <div class="input-group mb-3">
+                        <select name="tables[][id]" class="form-control">
+                            @foreach ($tables as $table)
+                                <option value="{{ $table->id }}">{{ $table->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <div class="input-group-append">
+                        <button id="removeRow" type="button"
+                                class="btn btn-danger">Remove</button>
+                        </div>
+                    </div>
+                </div>`;
+    $('#newRow').append(html);
+});
+
+// remove row
+$(document).on('click', '#removeRow', function () {
+    $(this).closest('#inputFormRow').remove();
+});
+
+var x = 0;
+$("#addRow1").click(function () {
+    var html = '';
+    x++;
+    html += `<div id="inputFormRow1">
+                    <div class="input-group mb-3">
+                        <select name="dishes[${x}][id]" class="form-control">
+                            @foreach ($dishes as $dish)
+                                <option value="{{ $dish->id }}">{{ $dish->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <input type="number" name="dishes[${x}][qty]" class="form-control">
+                        <input type="hidden" name="dishes[${x}][price]" class="form-control" value="{{ $dish->price }}">
+                        <div class="input-group-append">
+                            <button id="removeRow1" type="button"
+                                class="btn btn-danger">Remove</button>
+                        </div>
+                    </div>
+                 </div>`;
+    $('#newRow1').append(html);
+});
+
+// remove row
+$(document).on('click', '#removeRow1', function () {
+    $(this).closest('#inputFormRow1').remove();
+});
